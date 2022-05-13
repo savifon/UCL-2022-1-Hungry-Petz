72f650896a474a9147bef50a22dc574a3e977b6f
Aquiles Aguiar <67012626+AquilesAguiar@users.noreply.github.com>
2022-05-13 09:05:41 -0300

    Update app and controla motor

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template # These are all we need for our purposes
-from controlaMotor import *
+from flask import Flask, render_template, request
+#from controlaMotor import *
 from time import sleep
 from flask_cors import CORS
 
@@ -8,13 +8,30 @@ CORS(app)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    print("Entrou")
     return render_template("index.html")
     
 @app.route("/liberaRacao", methods=["GET", "POST"])
 def liberaRacao():
-    forward(3)
-    sleep(4)
-    stop()
+    motorGiro = request.json()
+    if motorGiro["cart"][0]["id"] == 1 and motorGiro["cart"][1]["id"] == 2:
+        # forward1(3)
+        # forward2(3)
+        # sleep(4)
+        # stop1()
+        # stop2()
+        print(1)
+    elif motorGiro["cart"][0]["id"] == 1:
+        # forward1(3)
+        # sleep(4)
+        # stop1()
+        print(2)
+
+    elif motorGiro["cart"][1]["id"] == 2:
+        # forward2(3)
+        # sleep(4)
+        # stop2()
+        print(3)
     return {"liberaRacao":True}
 
 app.run(port=5587)
--- controlaMotor.py
+++ controlaMotor.py
@@ -4,26 +4,47 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 enable_pin = 13
-in_1_pin = 5
-in_2_pin = 6
+in_1_pin = 6
+in_2_pin = 5
+
+in_3_pin = 16
+in_4_pin = 26
 
 GPIO.setup(enable_pin, GPIO.OUT)
 GPIO.setup(in_1_pin, GPIO.OUT)
 GPIO.setup(in_2_pin, GPIO.OUT)
+GPIO.setup(in_3_pin, GPIO.OUT)
+GPIO.setup(in_4_pin, GPIO.OUT)
+
 motor_pwm = GPIO.PWM(enable_pin, 500)
 motor_pwm.start(0)
 
-def forward(duty):
+def forward1(duty):
     GPIO.output(in_1_pin, True) 
     GPIO.output(in_2_pin, False) 
     motor_pwm.ChangeDutyCycle(duty)
 
-def reverse(duty):
+def reverse1(duty):
     GPIO.output(in_1_pin, False) 
     GPIO.output(in_2_pin, True) 
     motor_pwm.ChangeDutyCycle(duty)
 
-def stop():
+def stop1():
     GPIO.output(in_1_pin, False) 
     GPIO.output(in_2_pin, False) 
+    motor_pwm.ChangeDutyCycle(0)
+
+def forward2(duty):
+    GPIO.output(in_3_pin, True) 
+    GPIO.output(in_4_pin, False) 
+    motor_pwm.ChangeDutyCycle(duty)
+
+def reverse2(duty):
+    GPIO.output(in_3_pin, False) 
+    GPIO.output(in_4_pin, True) 
+    motor_pwm.ChangeDutyCycle(duty)
+
+def stop2():
+    GPIO.output(in_3_pin, False) 
+    GPIO.output(in_4_pin, False) 
     motor_pwm.ChangeDutyCycle(0)
